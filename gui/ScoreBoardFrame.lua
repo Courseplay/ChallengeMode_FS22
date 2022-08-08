e35b24376299c5aea227ad6ec9220937883f64f1
tn4799 <tn4799@gmail.com>
2022-08-08 14:08:10 +0200

    added another element to list when in admin mode

--- gui/ScoreBoardFrame.lua
+++ gui/ScoreBoardFrame.lua
@@ -25,7 +25,7 @@ ScoreBoardFrame = {
 		SETTINGS = 2
 	},
 	NUM_SETTINGS = 1,
-	NUM_SETTINGS_ADMIN = 2,
+	NUM_SETTINGS_ADMIN = 3,
 	NUM_LEFT_SECTIONS = 2
 }
 
@@ -34,6 +34,7 @@ ScoreBoardFrame.translations = {
 
 	ruleTitle = g_i18n:getText("CM_leftList_ruleTitle"),
 	adminPointsTitle = g_i18n:getText("CM_leftList_adminPointsTitle"),
+	goalTitle = g_i18n:getText("CM_leftList_goalSettings"),
 	menuButtons = {
 		adminLogin = g_i18n:getText("CM_menuBtn_admin_login"),
 		adminLogout = g_i18n:getText("CM_menuBtn_admin_logout"),
@@ -154,7 +155,7 @@ function ScoreBoardFrame:onGuiSetupFinished()
 		[self.leftList] = function() return self.NUM_LEFT_SECTIONS end,
 		[self.rightList] = function()
 			local sx, ix = self.leftList:getSelectedPath()
-			return self.managers[sx]():getNumberOfElements()
+			return ix <= 2 and self.managers[sx]():getNumberOfElements() or 1
 		end
 	}
 	self.sectionTitles = {
@@ -228,14 +229,19 @@ function ScoreBoardFrame:getNumberOfItemsInSection(list, section)
 		end
 	else
 		local sx, ix = self.leftList:getSelectedPath()
-		local farmId = self:getCurrentFarmId()
-		local l = self.managers[sx](farmId)
-		if l == nil then
-			CmUtil.debug("Categories for not found %d", section)
-			printCallstack()
-			return 0
+
+		if ix <= 2 then
+			local farmId = self:getCurrentFarmId()
+			local l = self.managers[sx](farmId)
+			if l == nil then
+				CmUtil.debug("Categories for not found %d", section)
+				printCallstack()
+				return 0
+			end
+			return l:getNumberOfElements(section)
+		else
+			return 1
 		end
-		return l:getNumberOfElements(section)
 	end
 end
 
@@ -261,17 +267,23 @@ function ScoreBoardFrame:populateCellForItemInSection(list, section, index, cell
 		elseif index == 1 then
 			cell:getAttribute("title"):setText(self.translations.ruleTitle)
 			cell:getAttribute("icon"):setVisible(false)
-		else
+		elseif index == 2 then
 			cell:getAttribute("title"):setText(self.translations.adminPointsTitle)
+			cell:getAttribute("icon"):setVisible(false)
+		else
+			cell:getAttribute("title"):setText(self.translations.goalTitle)
 			cell:getAttribute("icon"):setVisible(false)
 		end
 	else
 		local element = self:getElement(section, index)
 		if element then
+			print("className: " .. tostring(element.className))
 			cell:getAttribute("title"):setText(element:getTitle())
 
+			print("value: " .. tostring(element:getText()))
 			cell:getAttribute("value"):setText(element:getText())
 
+			print("conversionValue: " .. tostring(element:getFactorText()))
 			cell:getAttribute("conversionValue"):setText(element:getFactorText())
 		end
 	end
@@ -323,13 +335,18 @@ function ScoreBoardFrame:getElement(section, index)
 	end
 	local sx, ix = self.leftList:getSelectedPath()
 	local farmId = self:getCurrentFarmId()
-	local list = self.managers[sx](farmId)
-	if list == nil then
-		CmUtil.debug("Element not found for (%s|%s).", tostring(section), tostring(index))
-		printCallstack()
-		return
+	if ix <= 2 then
+		local list = self.managers[sx](farmId)
+		if list == nil then
+			CmUtil.debug("Element not found for (%s|%s).", tostring(section), tostring(index))
+			printCallstack()
+			return
+		end
+
+		return list:getElement(section, index)
+	else
+		return GoalElement.new(g_i18n:getText(self.translations.goalTitle), self.translations.goalTitle, self.victoryPointManager:getGoal())
 	end
-	return list:getElement(section, index)
 end
 
 ----------------------------------------------------
