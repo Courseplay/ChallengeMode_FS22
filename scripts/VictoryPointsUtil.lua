39c74e07c9be693211640cfac635200bf71e3556
tn4799 <tn4799@gmail.com>
2022-09-22 15:30:21 +0200

    implemented merge of all fillTypes

--- scripts/VictoryPointsUtil.lua
+++ scripts/VictoryPointsUtil.lua
@@ -40,7 +40,7 @@ function VictoryPointsUtil.getStorageAmount(farmId, maxFillLevel)
 	return totalFillLevels
 end
 
-function VictoryPointsUtil.getBaleAmount(farmId, maxFillLevel)
+function VictoryPointsUtil.getBaleAmount(farmId, maxFillLevel, totalFillLevels)
 	if farmId == nil then 
 		local fillTypes = {}
 		for i, bale in pairs(g_baleManager.bales) do
@@ -51,36 +51,34 @@ function VictoryPointsUtil.getBaleAmount(farmId, maxFillLevel)
 		return fillTypes
 	end
 	maxFillLevel = maxFillLevel == Rule.MAX_FILL_LEVEL_DISABLED and math.huge or maxFillLevel
-	local baleFillLevels = {}
 	for _, object in pairs(g_currentMission.nodeToObject) do
 		if object:isa(Bale) and object:getOwnerFarmId(farmId) == farmId and not object.isMissionBale then 
-			if baleFillLevels[object.fillType] == nil then 
-				baleFillLevels[object.fillType] = 0
+			if totalFillLevels[object.fillType] == nil then 
+				totalFillLevels[object.fillType] = 0
 			end
-			baleFillLevels[object.fillType] = baleFillLevels[object.fillType] + math.min(object.fillLevel, maxFillLevel)
+			totalFillLevels[object.fillType] = totalFillLevels[object.fillType] + math.min(object.fillLevel, maxFillLevel)
 		end
 	end
-	return baleFillLevels
+	return totalFillLevels
 end
 
-function VictoryPointsUtil.getPalletAmount(farmId, maxFillLevel)
+function VictoryPointsUtil.getPalletAmount(farmId, maxFillLevel, totalFillLevels)
 	if farmId == nil then 
 		return VictoryPointsUtil.getFillTypes()
 	end
 	maxFillLevel = maxFillLevel == Rule.MAX_FILL_LEVEL_DISABLED and math.huge or maxFillLevel
-	local palletFillLevels = {}
 	for _, object in pairs(g_currentMission.vehicles) do
 		if object.spec_pallet and object:getOwnerFarmId(farmId) == farmId then
 			local fillUnitIndex = object.spec_pallet.fillUnitIndex
 			local fillLevel = object:getFillUnitFillLevel(fillUnitIndex)
 			local fillType = object:getFillUnitFillType(fillUnitIndex)
-			if palletFillLevels[fillType] == nil then 
-				palletFillLevels[fillType] = 0
+			if totalFillLevels[fillType] == nil then 
+				totalFillLevels[fillType] = 0
 			end
-			palletFillLevels[fillType] = palletFillLevels[fillType] + math.min(fillLevel, maxFillLevel)
+			totalFillLevels[fillType] = totalFillLevels[fillType] + math.min(fillLevel, maxFillLevel)
 		end
 	end
-	return palletFillLevels
+	return totalFillLevels
 end
 
 function VictoryPointsUtil.getAnimalAmount(farmId, maxNumberOfAnimals)
