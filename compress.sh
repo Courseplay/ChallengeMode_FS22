3180f47d5314af7934d14d41c51d63ea8153ba0f
tn47 <tn4799@gmail.com>
2023-08-09 23:32:02 +0200

    fixed compress script
    script requires that zip-command is installed

--- compress.sh
+++ compress.sh
@@ -1,45 +1,21 @@
-#!/bin/bash
-
-# Maybe you have to enter "Set-ExecutionPolicy Unrestricted" in Powershell (started with admin rights), because Win10 does not allow the use of non Win10 Powershell scripts without it. :'(
-# I used Compress-Archive with a temporary folder because Compress-Archive ignores the specified folder
-
-# Files/Directories to zip
-declare -a fd_toZip=(
-	"modDesc.xml"
-	"ChallengeModConfig.xml"
-	"Icon_ChallengeMode.dds"
-	"events/"
-	"gui/"
-	"scripts/"
-	"translations/"
-)
-
-declare zip_name="FS22_ChallengeMode.zip"
-
-# delete old temp folder + create new one
-[[ -e "temp-zip-folder/" ]] && rm -rf "temp-zip-folder/"
-mkdir -p "temp-zip-folder/"
-
-# copy files/directorys in temp zip folder
-for fd in "${fd_toZip[@]}"
-do
-	if test -f "$fd"; then	# file
-		cp $fd temp-zip-folder/
-	elif [ -d "$fd" ]; then	# directory
-		cp -r $fd temp-zip-folder/$fd
-	fi
-done
+zip FS22_ChallengeMode.zip modDesc.xml ChallengeModConfig.xml Icon_ChallengeMode.dds events/* gui/* gui/dialogs/* scripts/* translations/*
+mv FS22_ChallengeMode.zip ..
 
 
-# delete old zip folder if exists
-[[ -e "$zip_name" ]] && rm -f "$zip_name"
-# zip temp folder
-powershell "Compress-Archive -Path temp-zip-folder/* -DestinationPath $zip_name"
+#$compress = @{
+#	Path = "modDesc.xml", ".\gui\", ".\events\", ".\scripts\", ".\translations\", "ChallengeModConfig.xml", "Icon_ChallengeMode.dds"
+#	DestinationPath = "FS22_ChallengeMode.zip"
+#}
+#powershell Compress-Archive -Confirm @compress
+#mv FS22_ChallengeMode.zip ..
 
-# delete temp zip folder
-[[ -e "temp-zip-folder/" ]] && rm -rf "temp-zip-folder/"
 
-# confirm execution
-echo "zip is done"
-mv "$zip_name" ..
-#exec $SHELL		# remove this if you want the console to close on finish
+#powershell Compress-Archive modDesc.xml -Update FS22_ChallengeMode.zip
+#powershell Compress-Archive -Path Icon_ChallengeMode.dds -Update -DestinationPath FS22_ChallengeMode.zip
+#powershell Compress-Archive -Path ChallengeModConfig.xml -Update -DestinationPath FS22_ChallengeMode.zip
+#powershell Compress-Archive -Path $(powershell Get-ChildItem -Path events) -Update -DestinationPath FS22_ChallengeMode.zip
+#powershell Get-ChildItem -Path gui |
+#powershell Compress-Archive -Path gui/* -Update -DestinationPath FS22_ChallengeMode.zip
+#powershell Get-ChildItem -Path scripts | powershell Compress-Archive -Update -DestinationPath FS22_ChallengeMode.zip
+#powershell Get-ChildItem -Path translations | powershell Compress-Archive -Update -DestinationPath FS22_ChallengeMode.zip
+#mv FS22_ChallengeMode.zip ..
