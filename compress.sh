2f6b5ebb5b674bf32c5de5d0902036e1e2d1c475
Tim Nolle <tim.nolle@uni-ulm.de>
2023-08-10 14:41:32 +0200

    made compress script modular

--- compress.sh
+++ compress.sh
@@ -1,21 +1,11 @@
-zip FS22_ChallengeMode.zip modDesc.xml ChallengeModConfig.xml Icon_ChallengeMode.dds events/* gui/* gui/dialogs/* scripts/* translations/*
-mv FS22_ChallengeMode.zip ..
+set -e
 
+$ZIP_NAME="FS22_ChallengeMode.zip"
 
-#$compress = @{
-#	Path = "modDesc.xml", ".\gui\", ".\events\", ".\scripts\", ".\translations\", "ChallengeModConfig.xml", "Icon_ChallengeMode.dds"
-#	DestinationPath = "FS22_ChallengeMode.zip"
-#}
-#powershell Compress-Archive -Confirm @compress
-#mv FS22_ChallengeMode.zip ..
+# list all files that should be part of the zip archive
+# seperate each file with a ' '. for each folder that you want to zip add a '/*' to the end.
+# this means that you want to zip all files in that folder
+$FILES_TO_ZIP="modDesc.xml ChallengeModConfig.xml Icon_ChallengeMode.dds events/* gui/* gui/dialogs/* scripts/* translations/*"
 
-
-#powershell Compress-Archive modDesc.xml -Update FS22_ChallengeMode.zip
-#powershell Compress-Archive -Path Icon_ChallengeMode.dds -Update -DestinationPath FS22_ChallengeMode.zip
-#powershell Compress-Archive -Path ChallengeModConfig.xml -Update -DestinationPath FS22_ChallengeMode.zip
-#powershell Compress-Archive -Path $(powershell Get-ChildItem -Path events) -Update -DestinationPath FS22_ChallengeMode.zip
-#powershell Get-ChildItem -Path gui |
-#powershell Compress-Archive -Path gui/* -Update -DestinationPath FS22_ChallengeMode.zip
-#powershell Get-ChildItem -Path scripts | powershell Compress-Archive -Update -DestinationPath FS22_ChallengeMode.zip
-#powershell Get-ChildItem -Path translations | powershell Compress-Archive -Update -DestinationPath FS22_ChallengeMode.zip
-#mv FS22_ChallengeMode.zip ..
+zip "$ZIP_NAME" "$FILES_TO_ZIP"
+mv "$ZIP_NAME" ..
